97a1966c20b4217f599f7da729ab0129e6818359
Alexander <alexander891622gmail.com>
2025-11-20 22:02:58 -0800

    Added clone constructor and support for null UVs

--- UnBox3D/Models/DMesh3WithTextures.cs
+++ UnBox3D/Models/DMesh3WithTextures.cs
@@ -9,12 +9,31 @@ namespace UnBox3D.Models
     /// </summary>
     public class DMesh3WithTextures : DMesh3
     {
-        public Vector2f[] UVsArray { get; private set; }
+        public Vector2f[]? UVsArray { get; private set; }
         public string DiffuseTexturePath { get; set; }
+
+        // Default constructor
         public DMesh3WithTextures(string diffuseTexturePath = "") : base()
         {
             UVsArray = Array.Empty<Vector2f>(); // initialize empty array for safety
             DiffuseTexturePath = diffuseTexturePath;
+        }
+
+        // Copy constructor
+        public DMesh3WithTextures(DMesh3WithTextures copy) : base(copy)
+        {
+            // Deep copy UVs
+            if (copy.UVsArray != null)
+            {
+                UVsArray = new Vector2f[copy.UVsArray.Length];
+                Array.Copy(copy.UVsArray, UVsArray, copy.UVsArray.Length);
+            }
+            else
+            {
+                UVsArray = null;
+            }
+
+            DiffuseTexturePath = copy.DiffuseTexturePath;
         }
 
         // Resize the UVs array to match the number of vertices
@@ -26,11 +45,14 @@ namespace UnBox3D.Models
         // Set UV coordinate for a specific vertex
         public void SetUV(int i, Vector2d uv)
         {
+            if (UVsArray != null)
+            {
             // safety: ensure array is large enough
             if (UVsArray.Length != VertexCount)
                 AllocateUVs();
 
             UVsArray[i] = new Vector2f((float)uv.x, (float)uv.y);
+            }
         }
     }
 }
