cdd97dbf027a4ed0398dfcf78e2493c2f605abd9
Michael Ramos <124205129+mramosr2@users.noreply.github.com>
2025-11-20 22:55:11 -0800

    Update Summary for Help Window

--- UnBox3D/Views/HelpWindow.xaml.cs
+++ UnBox3D/Views/HelpWindow.xaml.cs
@@ -14,28 +14,36 @@ using System.Windows.Shapes;
 
 namespace UnBox3D.Views
 {
-    /// <summary>
-    /// Interaction logic for HelpWindow.xaml
-    /// </summary>
+    // interaction logic for HelpWindow.xaml
     public partial class HelpWindow : Window
     {
         private readonly IServiceProvider _services;
+
+        // constructor: initialize UI components from xaml and assigns services to local field
         public HelpWindow(IServiceProvider services)
         {
             InitializeComponent();
             _services = services;
         }
+
+        // this function is the logic for close button
         private void Close_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
+        // this function is the logic for the back button which creates a main menu window and displays it
+        // then close the pop up help window
         private void Back_Click(object sender, RoutedEventArgs e)
         {
             var mainMenu = new MainMenuWindow(_services);
             mainMenu.Show();
             this.Close();
         }
+
+        // this function overrides the windows default key handling in order to add the 'esc' feature
+        // when user presses 'esc' key, it opens the main menu window and closes the help window
+        // similar to the back_click function above
         protected override void OnKeyDown(System.Windows.Input.KeyEventArgs e)
         {
             base.OnKeyDown(e);
